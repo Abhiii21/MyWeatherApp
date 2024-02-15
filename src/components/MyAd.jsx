8316e7d6a7ddc511fdcf0f5e0031554969d5afcb
Abhijeet Kumar <aryan.abhijeet21@gmail.com>
2024-02-15 11:01:42 +0530

    first

---
+++ src/components/MyAd.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+const MyAd = () => {
+  useEffect(() => {
+    <script
+      async
+      src="https://js.onclckmn.com/static/adManager.js"
+      data-admpid="149027"
+    ></script>;
+    <script
+      async
+      src="https://js.onclckmn.com/static/adManager.js"
+      data-admpid="149035"
+    ></script>;
+  }, []);
+
+  return null;
+};
+
+export default MyAd;
